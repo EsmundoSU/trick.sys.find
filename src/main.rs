d3c159c77acab9fc18e3a92f4e3331143eff8421
Adam Pelc <adam.pelc.su@gmail.com>
2022-02-13 22:55:32 +0100

    feat: Added Clap module for args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::{App, AppSettings, Parser};
+
+#[derive(Parser)]
+struct Args {
+    #[clap(long)]
+    search_type: String,
+
+    #[clap(parse(from_os_str))]
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let _app = App::new("tricksys-find")
+        .author("Adam Pelc, adam.pelc.su@gmail.com")
+        .version("0.1.0")
+        .about("Versatile system utility designed to: search for files or search the contents of files in a simple and intuitive way.")
+        .bin_name("tricksys-find")
+        .setting(AppSettings::SubcommandRequired)
+        .subcommand(App::new("file").about("Search type: is looking for single file"))
+        .subcommand(App::new("files").about("Search type: is looking for multiple files"))
+        .subcommand(App::new("content").about("Search type: is looking for some content is file(s)"));
 }
