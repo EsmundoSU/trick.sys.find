4f51a5d7221c33455a4e043959024b291fea0125
Adam Pelc <adam.pelc.su@gmail.com>
2022-03-01 21:36:38 +0100

    added basic app builder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+mod trick_sys {
+    pub mod find {
+        pub struct App {
+            name: String,
+        }
+
+        impl App {
+            pub fn run(&self) {
+                println!("App: {} started", self.name);
+            }
+        }
+
+        pub struct AppBuilder {
+            app: App,
+        }
+
+        impl AppBuilder {
+            pub fn new(app_name: String) -> AppBuilder {
+                AppBuilder {
+                    app: App {
+                        name: app_name,
+                    }
+                }
+            }
+
+            pub fn build(self) -> App {
+                println!("App: {} has been build", self.app.name);
+                self.app
+            }
+        }
+    }
+}
+
+use trick_sys::find::AppBuilder;
+
 fn main() {
-    println!("Hello, world!");
+    let app_builder = AppBuilder::new("tricksys-find".to_string());
+    let app = app_builder.build();
+
+    app.run();
 }
